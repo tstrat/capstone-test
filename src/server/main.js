ff114d23a4cc95cd64afffaee7b4e9e518a535ed
Travis Stratton <t.strat7@gmail.com>
2024-03-28 09:35:16 -0600

    use PORT env

--- src/server/main.js
+++ src/server/main.js
@@ -15,7 +15,10 @@ db.connect()
 const apiRouter = require('./api');
 app.use('/api', apiRouter);
 
-router.listen(app, 3000, () =>
+const PORT = process.env.PORT || 3000
+
+
+router.listen(app, PORT, () =>
   console.log('Server is listening on port 3000...')
 );
 
